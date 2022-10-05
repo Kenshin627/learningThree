71d4b53cfd1d0929b5e3103818bd597d04d19027
kenshin627 <87zhaoxiaobo@163.com>
2022-10-05 23:31:59 +0800

    box

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,27 @@ const material = new THREE.MeshBasicMaterial({
 
 const cube = new THREE.Mesh(box, material);
 scene.add(cube);
+
+//camera
+const cameraConfig = {
+  fov: 75,
+  width: 800,
+  height: 600,
+  near: 0.1,
+  far: 2000
+}
+const camera = new THREE.PerspectiveCamera(cameraConfig.fov, cameraConfig.width / cameraConfig.height, cameraConfig.near, cameraConfig.far);
+camera.position.z = 2;
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector("#container") as HTMLCanvasElement;
+
+const rendererOpts: THREE.WebGLRendererParameters = {
+  canvas
+};
+const renderer = new THREE.WebGLRenderer(rendererOpts)
+
+renderer.setSize(cameraConfig.width, cameraConfig.height);
+
+renderer.render(scene, camera);
